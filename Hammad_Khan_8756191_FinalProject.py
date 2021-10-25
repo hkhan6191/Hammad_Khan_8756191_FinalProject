1db80ad872f61ab88557c13556edb44917bbc29a
HAMMAD KHAN <hkhan6191@conestogac.on.ca>
2021-10-25 12:10:54 -0400

    init

---
+++ Hammad_Khan_8756191_FinalProject.py
@@ -0,0 +1,18 @@
+import random
+
+userWins = 0
+compWins = 0
+
+choicesList = ["rock", "paper", "scissors"]
+
+print("Welcome to Rock-Paper-Scissors! Your goal is to beat the computer. Remember: Rock beats Scissors, Paper beats Rock, and Scissors beat Paper.")
+
+while True:
+    userChoice = input("Enter in Rock, Paper, or Scissors. Enter in 'end' to finish the game.")
+    
+    if userChoice == "end":
+        break
+
+    elif userChoice not in choicesList:
+        print("Oops! Please choose either rock paper or scissors please.")
+        continue 
