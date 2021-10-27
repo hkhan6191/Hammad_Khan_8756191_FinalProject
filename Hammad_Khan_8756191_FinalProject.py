331668ffb5138e83a445645cfddbccdcb9fa445a
HAMMAD KHAN <hkhan6191@conestogac.on.ca>
2021-10-27 10:34:55 -0400

    sec

--- Hammad_Khan_8756191_FinalProject.py
+++ Hammad_Khan_8756191_FinalProject.py
@@ -8,11 +8,16 @@ choicesList = ["rock", "paper", "scissors"]
 print("Welcome to Rock-Paper-Scissors! Your goal is to beat the computer. Remember: Rock beats Scissors, Paper beats Rock, and Scissors beat Paper.")
 
 while True:
-    userChoice = input("Enter in Rock, Paper, or Scissors. Enter in 'end' to finish the game.")
+    userChoice = input("Enter in Rock, Paper, or Scissors. Enter in 'end' to finish the game. ")
     
     if userChoice == "end":
         break
 
     elif userChoice not in choicesList:
-        print("Oops! Please choose either rock paper or scissors please.")
+        print("Oops! Please choose either rock, paper, or scissors please.")
         continue 
+
+    randomNum = random.randint(0,2)
+    comChoice = choicesList[randomNum]
+
+    print("Computer chose: ", comChoice) 
