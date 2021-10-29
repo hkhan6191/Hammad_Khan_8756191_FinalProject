e4e26c0245f473f0a4ac78d00b6e002eea908e6f
HAMMAD KHAN <hkhan6191@conestogac.on.ca>
2021-10-29 11:48:04 -0400

    fourth

--- Hammad_Khan_8756191_FinalProject.py
+++ Hammad_Khan_8756191_FinalProject.py
@@ -1,4 +1,5 @@
 import random
+#Python module
 
 userWins = 0
 compWins = 0
@@ -7,32 +8,45 @@ choicesList = ["rock", "paper", "scissors"]
 
 print("Welcome to Rock-Paper-Scissors! Your goal is to beat the computer. Remember: Rock beats Scissors, Paper beats Rock, and Scissors beat Paper.")
 
+print("")
+
 while True:
-    userChoice = input("Enter in Rock, Paper, or Scissors. Enter in 'end' to finish the game. ")
+    userChoice = input("Enter in rock, paper, or scissors as your choice. Enter in 'end' if you wish to finish the game.")
     
     if userChoice == "end":
         break
 
     elif userChoice not in choicesList:
-        print("Oops! Please choose either rock, paper, or scissors please.")
+        print("Oops! Please choose either 'rock', 'paper', 'scissors', or 'end' in lowercase please.")
         continue 
 
     randomNum = random.randint(0,2)
+    #0 = rock     1 = paper     2 = scissors
     comChoice = choicesList[randomNum]
     print("Computer chose: ", comChoice) 
 
-    if userChoice == "Rock" and comChoice == "Scissors":
-        print("Rock beats Scissors")
+    if userChoice == "rock" and comChoice == "scissors":
+        print("You win!")
         userWins += 1
 
-    elif userChoice == "Paper" and comChoice == "Rock":
-        print("Paper beats Rock")
+    elif userChoice == "paper" and comChoice == "rock":
+        print("You win!")
         userWins += 1
 
-    elif userChoice == "Scissors" and comChoice == "Paper":
-        print("Scissors beats Paper")
+    elif userChoice == "scissors" and comChoice == "paper":
+        print("You win!")
         userWins += 1
+
+    elif userChoice == comChoice:
+        print("Tie!")
 
     else:
-        print(comChoice, "beats", userChoice)
+        print("You lose!")
         compWins += 1 
+
+print("You won", userWins, "games.")
+print("The computer won", compWins, "games.")
+
+print("")
+
+print("See you later!") 
